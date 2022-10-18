4e51016e50bf149c117578e9a898c134ccd42600
jschaefmn <jschaefmn@gmail.com>
2022-10-18 10:03:17 -0500

    working on routes

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+  console.log('Connected to MongoDB');
+});
+
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+app.listen(8800, () => {
+  console.log('Server is running on port 8800');
+});
