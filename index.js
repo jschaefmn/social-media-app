08a7f7cfb1fac371cc3ab2fb32a90cadb8da6c35
jschaefmn <jschaefmn@gmail.com>
2022-10-18 11:05:09 -0500

    creating heroku app

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
@@ -15,6 +18,9 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopol
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Server is running on port 8800');
