c8965abab7bd2b66e20130150d6c3f6ef61f3e19
jschaefmn <jschaefmn@gmail.com>
2022-10-20 09:43:43 -0500

    having issue with react server

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,7 +1,17 @@
-import Topbar from "../../components/topbar/Topbar";
+import Feed from '../components/feed/Feed';
+import Rightbar from '../components/rightbar/Rightbar';
+import Sidebar from '../components/sidebar/Sidebar';
+import Topbar from '../components/topbar/Topbar';
 
 export default function Home() {
   return (
-    <Topbar />
-  )
+    <>
+      <Topbar />
+      <div className='homeContainer'>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 }
