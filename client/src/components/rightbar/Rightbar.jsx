bcc6979bebc87a841392a5c9e66d475be6ceb9e0
jschaefmn <jschaefmn@gmail.com>
2022-10-27 08:57:32 -0500

    finished styling the website

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -6,7 +6,7 @@ export default function Rightbar({ profile }) {
   const HomeRightbar = () => {
     return (
       <>
-      <div className='birthdayContainer'>
+        <div className='birthdayContainer'>
           <img className='birthdayImg' src='/assets/gift.png' alt='' />
           <span className='birthdayText'>
             <b>Example name</b> and <b>3 others</b> have birthdays today
@@ -20,42 +20,42 @@ export default function Rightbar({ profile }) {
           ))}
         </ul>
       </>
-    )
-  }
+    );
+  };
 
   const ProfileRightbar = () => {
     return (
       <>
-        <h4 className="rightbarTitle">User Information</h4>
-        <div className="rightbarInfo">
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">Omaha</span>
+        <h4 className='rightbarTitle'>User Information</h4>
+        <div className='rightbarInfo'>
+          <div className='rightbarInfoItem'>
+            <span className='rightbarInfoKey'>City:</span>
+            <span className='rightbarInfoValue'>Omaha</span>
           </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">Eagan</span>
+          <div className='rightbarInfoItem'>
+            <span className='rightbarInfoKey'>From:</span>
+            <span className='rightbarInfoValue'>Eagan</span>
           </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">In a relationship</span>
+          <div className='rightbarInfoItem'>
+            <span className='rightbarInfoKey'>Relationship:</span>
+            <span className='rightbarInfoValue'>In a relationship</span>
           </div>
         </div>
-        <h4 className="rightbarTitle">User friends</h4>
-        <div className="rightbarFollowings">
-          <div className="rightbarFollowing">
-            <img src="/assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
-            <span className="rightbarFollowingName">John Carter</span>
+        <h4 className='rightbarTitle'>User friends</h4>
+        <div className='rightbarFollowings'>
+          <div className='rightbarFollowing'>
+            <img src='/assets/person/1.jpeg' alt='' className='rightbarFollowingImg' />
+            <span className='rightbarFollowingName'>John Carter</span>
           </div>
         </div>
       </>
-    )
-  }
+    );
+  };
 
   return (
     <div className='rightbar'>
       <div className='rightbarWrapper'>
-        <ProfileRightbar />
+        {profile} ? <ProfileRightbar /> : <HomeRightbar />
       </div>
     </div>
   );
